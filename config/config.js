f3748aca365e713f2e00267b6edd931d6b56528e
Ed <ed@polarity.io>
2019-03-07 11:50:43 -0500

    Set default log level back to info

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "ReversingLabs",
   acronym: "RL",
-  logging: { level: "trace" },
+  logging: { level: "info" },
   entityTypes: ["hash"],
   description: "ReversingLabs integration for real-time file hash lookups",
   styles: ["./styles/reversinglabs.less"],

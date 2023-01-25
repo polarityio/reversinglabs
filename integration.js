ccf776ebc9e33d6857e9597da9c6d615f58754dc
ntsmith7 <nick@polarity.io>
2023-01-24 16:20:33 -0800

    chore: Updated postman-request imports

--- integration.js
+++ integration.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const request = require('request');
+const request = require('postman-request');
 const async = require('async');
 const fs = require('fs');
 const config = require('./config/config');
